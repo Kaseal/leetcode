3fa9c0d806401e20a5518c65b8ac2392f0f7ea25
Kaseal <kashin.sergey.programmer@gmail.com>
2021-02-12 13:13:24 +0200

    SameTree refactored

--- src/main/java/easy/SameTree.java
+++ src/main/java/easy/SameTree.java
@@ -6,6 +6,18 @@ import easy.helper_classes.TreeNode;
 public class SameTree {
 
 	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+
+		if (p != null && q != null && p.val == q.val) {
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		}
+
+		return false;
+	}
+
+	public boolean _isSameTree(TreeNode p, TreeNode q) {
 		StringBuilder result1 = new StringBuilder();
 		StringBuilder result2 = new StringBuilder();
 
