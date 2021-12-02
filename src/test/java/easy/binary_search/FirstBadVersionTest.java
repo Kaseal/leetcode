edd34e493b7581e65dffe0b5b158c7604f193d3c
Kaseal <kashin.sergey.programmer@gmail.com>
2021-12-02 21:22:29 +0200

    Fixed maven test

--- src/test/java/easy/binary_search/FirstBadVersionTest.java
+++ src/test/java/easy/binary_search/FirstBadVersionTest.java
@@ -1,8 +1,8 @@
 package easy.binary_search;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -10,26 +10,26 @@ import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-class FirstBadVersionTest {
+public class FirstBadVersionTest {
 
 	@Spy
 	private FirstBadVersion firstBadVersion;
 
-	@BeforeEach
-	void setUp() {
+	@Before
+	public void setUp() {
 		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
-	void test1() {
+	public void test1() {
 		Mockito.doReturn(true).when(firstBadVersion).isBadVersion(4);
-		Assertions.assertEquals(4, firstBadVersion.firstBadVersion(5));
+		Assert.assertEquals(4, firstBadVersion.firstBadVersion(5));
 	}
 
 	@Test
-	void test2() {
+	public void test2() {
 		Mockito.doReturn(true).when(firstBadVersion).isBadVersion(1);
-		Assertions.assertEquals(1, firstBadVersion.firstBadVersion(1));
+		Assert.assertEquals(1, firstBadVersion.firstBadVersion(1));
 	}
 
 }
