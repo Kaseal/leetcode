bedafd1492e811e0d2f3850d089ae2061b6abdb9
Kaseal <kashin.sergey.programmer@gmail.com>
2021-12-11 09:32:40 +0200

    MergeTwoSortedLists, ReverseLinkedList Study Plan. Algorithm I. Day 10. Recursion / Backtracking

--- src/main/java/easy/lists/ReverseLinkedList.java
+++ src/main/java/easy/lists/ReverseLinkedList.java
@@ -2,10 +2,49 @@ package easy.lists;
 
 import common.helper_classes.ListNode;
 
+import java.util.Stack;
+
 //206. Reverse Linked List (Easy)
 public class ReverseLinkedList {
 
 	public ListNode reverseList(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		ListNode listNode = reverseList(head.next);
+
+		head.next.next = head;
+		head.next = null;
+
+		return listNode;
+	}
+
+	public ListNode __reverseList(ListNode head) {
+		ListNode res;
+
+		if (head == null) {
+			return null;
+		}
+
+		Stack<ListNode> stack = new Stack<>();
+		while (head != null) {
+			stack.push(head);
+			head = head.next;
+		}
+
+		res = stack.pop();
+		ListNode buf = res;
+		while (!stack.isEmpty()) {
+			buf.next = stack.pop();
+			buf = buf.next;
+		}
+		buf.next = null;
+
+		return res;
+	}
+
+	public ListNode _reverseList(ListNode head) {
 		if (head == null) {
 			return null;
 		}
